1087c3e99ecf3ff335e203dec73dc069ac3f5b8b
Christina Poole <codenamedchris@gmail.com>
2020-09-20 22:27:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -128,11 +128,11 @@ end
 
 def num_points_scored(player_name)
   # if
-    game_hash[:home][:players]#.each do |hash|
-      #hash #.each do |key, value|
+    game_hash[:home][:players].each do |hash|
+      hash.each do |key, value|
         #value == player_name
-        #value #hash[:points]
-      #end
+        value #hash[:points]
+      end
 
     #end
   # elsif
